9c9dd3b1fc472f35b7d0fc433fc935e7081cbefb
Jaydeep Deb <jaydeep.jaydeep.deb@gmail.com>
2020-03-19 13:28:39 +0530

    Added frontend validation

--- web/app.js
+++ web/app.js
@@ -8,20 +8,22 @@ app.controller('tableController', function ($scope, $http) {
     $scope.days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
     $scope.getTimeTableByClassName = function (className, adjust) {
-        adjust === true ? url = `http://localhost:3000/adjustTimeTable?class=${className}`
-            : url = `http://localhost:3000?class=${className}`;
-        $http.get(url)
-            .then(function (response) {
-                $scope.classWiseData = response.data.timeTable;
-                $scope.extraTeachersRequired = response.data.extraTeachersRequired;
-                $scope.heading = `Adjusted timetable for class ${className}`;
-                if (!adjust) {
-                    $scope.heading = `Timetable for class ${className}`;
-                    $scope.extraTeachersRequired = "";
-                    $scope.classWiseData = response.data;
-                }
-                $scope.classWiseTable = true;
-            });
+        if (className !== "") {
+            adjust === true ? url = `http://localhost:3000/adjustTimeTable?class=${className}`
+                : url = `http://localhost:3000?class=${className}`;
+            $http.get(url)
+                .then(function (response) {
+                    $scope.classWiseData = response.data.timeTable;
+                    $scope.extraTeachersRequired = response.data.extraTeachersRequired;
+                    $scope.heading = `Adjusted timetable for class ${className}`;
+                    if (!adjust) {
+                        $scope.heading = `Timetable for class ${className}`;
+                        $scope.extraTeachersRequired = "";
+                        $scope.classWiseData = response.data;
+                    }
+                    $scope.classWiseTable = true;
+                });
+        }
     }
 
 });
